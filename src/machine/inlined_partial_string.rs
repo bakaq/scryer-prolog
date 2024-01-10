8798f9aa8116ca2b423bd31e1144f66f190b46e0
bakaq <bakaq@bakaq.net>
2024-01-10 17:54:07 -0300

    InlinedBytes as_bytes(), as_str() and as_str_unchecked()

--- src/machine/inlined_partial_string.rs
+++ src/machine/inlined_partial_string.rs
@@ -1,18 +1,18 @@
 #![allow(dead_code)]
 
 use crate::machine::heap::*;
-use crate::types::*;
+//use crate::types::*;
 
 use std::marker::PhantomData;
 
 /// Bytes inlined in the heap as a 0 terminated contiguous array.
-/// 
+///
 /// I made a separate struct for this because this could be useful for
 /// more things than just partial strings.
 #[derive(Copy, Clone, Debug)]
 pub struct InlinedBytes<'a> {
     buffer: *const u8,
-    _phantom: PhantomData<&'a u8>
+    _phantom: PhantomData<&'a u8>,
 }
 
 impl InlinedBytes<'_> {
@@ -42,14 +42,53 @@ impl InlinedBytes<'_> {
             _phantom: PhantomData,
         }
     }
+
+    /// Return the length of the buffer.
+    ///
+    /// Note that this always traverses the entire buffer to find the sentinel.
+    fn len(&self) -> usize {
+        let mut len = 0;
+        loop {
+            // SAFETY: We know that the buffer is valid, so we will find a 0 and stop
+            // this loop before acessing memory outside of the buffer.
+            let val = unsafe { *self.buffer.add(len) };
+            if val == 0 {
+                break len;
+            }
+            len += 1;
+        }
+    }
+
+    /// Returns a slice to the bytes.
+    ///
+    /// Note that this always traverses the entire buffer to find the sentinel.
+    pub fn as_bytes(&self) -> &[u8] {
+        // SAFETY: The buffer is valid
+        unsafe { std::slice::from_raw_parts(self.buffer, self.len()) }
+    }
+
+    /// Interprets the bytes as a `&str` without checking UTF-8.
+    ///
+    /// Note that this always traverses the entire buffer to find the sentinel.
+    ///
+    /// # Safety
+    ///
+    /// The bytes must be valid UTF-8.
+    pub unsafe fn as_str_unchecked(&self) -> &str {
+        // SAFETY: The bytes are valid UTF-8.
+        unsafe { std::str::from_utf8_unchecked(self.as_bytes()) }
+    }
+
+    /// Interprets the bytes as a `&str` checking UTF-8.
+    ///
+    /// Note that this always traverses the entire buffer TWICE! One time to find
+    /// the sentinel, and another to check UTF-8.
+    pub fn as_str(&self) -> Result<&str, std::str::Utf8Error> {
+        std::str::from_utf8(self.as_bytes())
+    }
 }
 
 fn allocate_inlined_bytes<'a>(heap: &'a mut Heap, bytes: &[u8]) -> InlinedBytes<'a> {
-    // Ideally this should be dealt with before here to avoid using
-    // unnecessary memory, but this works with bytes.len() = 0 too.
-    #[cfg(not(test))]
-    debug_assert!(bytes.len() > 0, "why are you allocating 0 bytes???");
-
     let num_cells = bytes.len() / 8 + 1;
     let initial_len = heap.len();
 
@@ -57,15 +96,15 @@ fn allocate_inlined_bytes<'a>(heap: &'a mut Heap, bytes: &[u8]) -> InlinedBytes<
     let buffer = heap.spare_capacity_mut();
 
     // Sets the last cell as 0, this ensures the sentinel 0
-    buffer[num_cells-1].write(0.into());
+    buffer[num_cells - 1].write(0.into());
 
     // SAFETY: We have reserved all this memory above
-    unsafe { 
+    unsafe {
         // Copies all the the bytes of the string
         std::ptr::copy(bytes.as_ptr(), buffer.as_mut_ptr().cast(), bytes.len());
         heap.set_len(initial_len + num_cells);
     }
-    
+
     // SAFETY: We just put a valid buffer there
     unsafe { InlinedBytes::from_addr(&heap, initial_len) }
 }
@@ -87,15 +126,76 @@ mod test {
         assert_eq!(heap.len(), 1);
 
         // This should allocate only one cell
-        allocate_inlined_bytes(heap, &[1,2,3,4]);
+        allocate_inlined_bytes(heap, &[1, 2, 3, 4]);
         assert_eq!(heap.len(), 2);
 
         // This should allocate 2 cells, because of the sentinel
-        allocate_inlined_bytes(heap, &[1,2,3,4,5,6,7,8]);
+        allocate_inlined_bytes(heap, &[1, 2, 3, 4, 5, 6, 7, 8]);
         assert_eq!(heap.len(), 4);
 
         // This should also allocate 2 cells
-        allocate_inlined_bytes(heap, &[1,2,3,4,5,6,7,8,9,10]);
+        allocate_inlined_bytes(heap, &[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
         assert_eq!(heap.len(), 6);
+    }
+
+    #[test]
+    fn inlined_bytes_as_bytes() {
+        let mut wam = MockWAM::new();
+        let heap = &mut wam.machine_st.heap;
+
+        let test_bytes_list = [
+            &[] as &[u8],
+            &[1, 2, 3, 4],
+            &[1, 2, 3, 4, 5, 6, 7, 8],
+            &[1, 2, 3, 4, 5, 6, 7, 8, 9],
+        ];
+
+        for test_bytes in test_bytes_list {
+            let bytes = allocate_inlined_bytes(heap, test_bytes);
+            assert_eq!(bytes.as_bytes(), test_bytes);
+        }
+    }
+
+    #[test]
+    fn inlined_bytes_as_str() {
+        let mut wam = MockWAM::new();
+        let heap = &mut wam.machine_st.heap;
+
+        let valid_strs = ["", "1234", "12345678", "123456789", "漢字"];
+
+        for valid_str in valid_strs {
+            let str_bytes = valid_str.as_bytes();
+            let bytes = allocate_inlined_bytes(heap, str_bytes);
+            assert_eq!(bytes.as_str(), Ok(valid_str));
+        }
+
+        let invalid_strs: &[&[u8]] = &[
+            &[0xc3, 0x28],
+            &[0xa0, 0xa1],
+            &[0xe2, 0x28, 0xa1],
+            &[0xe2, 0x82, 0x28],
+            &[0xf0, 0x28, 0x8c, 0xbc],
+            &[0xf0, 0x90, 0x28, 0xbc],
+            &[0xf0, 0x28, 0x8c, 0x28],
+        ];
+
+        for invalid_str in invalid_strs {
+            let bytes = allocate_inlined_bytes(heap, invalid_str);
+            assert!(bytes.as_str().is_err());
+        }
+    }
+
+    #[test]
+    fn inlined_bytes_as_str_unchecked() {
+        let mut wam = MockWAM::new();
+        let heap = &mut wam.machine_st.heap;
+
+        let valid_strs = ["", "1234", "12345678", "123456789", "漢字"];
+
+        for valid_str in valid_strs {
+            let str_bytes = valid_str.as_bytes();
+            let bytes = allocate_inlined_bytes(heap, str_bytes);
+            assert_eq!(unsafe { bytes.as_str_unchecked() }, valid_str);
+        }
     }
 }
